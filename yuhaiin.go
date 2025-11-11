a1709f9061787b7e90693a4bee059ffac0ad4f55
Asutorufa <Asutorufa@users.noreply.github.com>
2025-11-11 03:52:06 +0000

    deploy: yuhaiin/yuhaiin-react@4bc59df6b1ce188e988868b1198339e54d7df173

--- yuhaiin.go
+++ yuhaiin.go
@@ -2,5 +2,5 @@ package yuhaiin_github_io
 
 import "embed"
 
-//go:embed 404 _next docs 404.html index.html index.txt
+//go:embed 404 _next _not-found docs 404.html index.html index.txt __next.__PAGE__.txt __next._full.txt __next._index.txt __next._tree.txt
 var Content embed.FS
