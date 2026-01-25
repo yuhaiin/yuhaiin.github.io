76424aa7dd4e5bc73fdc3726424965a829697402
Asutorufa <Asutorufa@users.noreply.github.com>
2026-01-25 16:03:46 +0000

    deploy: yuhaiin/yuhaiin-react@2751f03f63037ef669f3559aafbc5b50f91cf50c

--- yuhaiin.go
+++ yuhaiin.go
@@ -2,5 +2,5 @@ package yuhaiin_github_io
 
 import "embed"
 
-//go:embed 404 _next _not-found docs 404.html index.html index.txt __next.__PAGE__.txt __next._full.txt __next._index.txt __next._tree.txt
+//go:embed index.html assets docs
 var Content embed.FS
