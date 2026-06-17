461015040ed050134e3a539a8229d8ecc253c998
Asutorufa <Asutorufa@users.noreply.github.com>
2026-06-17 05:46:23 +0000

    deploy: yuhaiin/yuhaiin-react@1a6b33da28284e9fc6407dbfd63e48d5d397bab1

---
+++ assets/page-DEh4GHGk.js
@@ -0,0 +1,1 @@
+import{i as e}from"./motion-DN-q0qQi.js";import{m as t}from"./card-B1M_KIuh.js";import{t as n}from"./iframe-DtLdnTYG.js";var r=e(),i=()=>(0,r.jsx)(t,{children:(0,r.jsx)(n,{src:`https://yuhaiin.github.io/documents/`})});export{i as default};
