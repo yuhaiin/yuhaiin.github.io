b8528c15324f82e45f08b66e10b845e011692a9c
Asutorufa <Asutorufa@users.noreply.github.com>
2026-07-10 15:26:06 +0000

    deploy: yuhaiin/yuhaiin-react@d293d5e37976055718262a1ca5e0e2836ce22ad1

--- assets/page-Lv4hewSr2.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./react-BbvtWLgt.js";import{i as t}from"./motion-CZqih85E.js";import{t as n}from"./apiurl-Bpf1ffox.js";import{m as r}from"./card-FA950WCx.js";import{t as i}from"./iframe-CBs4KMrC.js";var a=t(),o=()=>{let[t]=e(n,``);return(0,a.jsx)(r,{children:(0,a.jsx)(i,{src:t+`/debug/pprof`,appearance:`light`})})};export{o as default};
