82b06b752cd68dbc9d2cc9d7cecabb4919b14ede
Asutorufa <Asutorufa@users.noreply.github.com>
2026-09-23 08:01:42 +0000

    deploy: yuhaiin/yuhaiin-react@801254f16d1d4d278d61127c19334dc71680f06a

--- assets/page-D928mR_f.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./react-DgY3ppiD.js";import{i as t}from"./motion-CsdcZwb_.js";import{t as n}from"./apiurl-DpLLnzry.js";import{m as r}from"./card-5nNrKJzM.js";import{t as i}from"./iframe-N7VAoNjQ.js";var a=t(),o=()=>{let[t]=e(n,``);return(0,a.jsx)(r,{children:(0,a.jsx)(i,{src:t+`/debug/pprof`,appearance:`light`})})};export{o as default};
